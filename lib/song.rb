1dc17ee7fe02f7f737e9be0292e86d25a1de69f2
Catherine Kawara <ndutakawara2@gmail.com>
2022-06-15 10:22:07 +0300

    Complete assignment

--- lib/song.rb
+++ lib/song.rb
@@ -49,4 +49,35 @@ class Song
     song.save
   end
 
+  def self.new_from_db(row)
+    # self.new is equivalent to Song.new
+    self.new(id: row[0], name: row[1], album: row[2])
+  end
+
+  # return all the songs in the database
+  def self.all
+    sql = <<-SQL
+      SELECT *
+      FROM songs
+    SQL
+
+    DB[:conn].execute(sql).map do |row|
+      self.new_from_db(row)
+    end
+  end
+
+  # searching for one  somg by name
+  def self.find_by_name(name)
+    sql = <<-SQL
+      SELECT *
+      FROM songs
+      WHERE name = ?
+      LIMIT 1
+    SQL
+
+    DB[:conn].execute(sql, name).map do |row|
+      self.new_from_db(row)
+      # grabbing the #first element from the returned array
+    end.first
+  end
 end
